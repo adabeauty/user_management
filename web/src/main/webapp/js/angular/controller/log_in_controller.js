49a55ea2857067b6bedbe64623dcb72f53126309
hawang <654296217@qq.com>
2015-05-08 11:09:51 +0800

    implement location

--- web/src/main/webapp/js/angular/controller/log_in_controller.js
+++ web/src/main/webapp/js/angular/controller/log_in_controller.js
@@ -1,17 +1,16 @@
 'use strict';
 
 angular.module('userManagement')
-    .controller('logInController', function($scope, $http){
+    .controller('logInController', function($scope, $http, $location){
 
-        $scope.administrator = {
-            name: 'admin',
-            password: '123456'
-        };
+        $scope.administrator = {};
 
         $scope.logIn = function(){
-            $http.post('/web/api/admins', {name: $scope.administrator.name, password:$scope.administrator.password}).success(function(data){
+            $http.post('/web/api/admins', {name: $scope.administrator.name, password:$scope.administrator.password}).success(function(result){
 
-                console.log(data);
+                if(JSON.parse(result)){
+                    $location.path('/');
+                }
             });
         };
     });
