607d9e67d608aa0997b1aca95052b12fbf92182e
hawang <654296217@qq.com>
2015-05-11 17:42:37 +0800

    solve display of userName in every page

--- web/src/main/webapp/js/angular/controller/resources_controller.js
+++ web/src/main/webapp/js/angular/controller/resources_controller.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('userManagement')
-    .controller('resourcesController', function(){
+    .controller('resourcesController', function($scope, $http){
 
+        log_out_nav_bar();
+
+        is_log_in();
+
+        function is_log_in(){
+
+            $http.get('/web/api/session').success(function(result){
+                if(result){
+                    $scope.Admin.name = result.user.name;
+                    $scope.logIn = true;
+                    $scope.logOut = false;
+                }
+            });
+        }
+
+        function log_out_nav_bar(){
+            $scope.Admin = {};
+            $scope.logIn = false;
+            $scope.logOut = true;
+        }
     });
