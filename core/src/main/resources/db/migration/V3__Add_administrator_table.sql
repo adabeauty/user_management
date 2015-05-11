9bf87bc4b430742318e17a77be837d1433d2267d
hawang <654296217@qq.com>
2015-05-11 11:26:46 +0800

    uncomment administrator table

--- core/src/main/resources/db/migration/V3__Add_administrator_table.sql
+++ core/src/main/resources/db/migration/V3__Add_administrator_table.sql
@@ -1,6 +1,6 @@
--- create table ADMINISTRATOR (
---     ID int not null AUTO_INCREMENT,
---     NAME varchar(100) not null,
---     PASSWORD varchar(100) not null,
---     PRIMARY KEY (ID)
--- );
+create table ADMINISTRATOR (
+    ID int not null AUTO_INCREMENT,
+    NAME varchar(100) not null,
+    PASSWORD varchar(100) not null,
+    PRIMARY KEY (ID)
+);
