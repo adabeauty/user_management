66312a972ad506e7cbda973703da7922a676cf50
hawang <654296217@qq.com>
2015-05-12 14:44:32 +0800

    create Item entity

---
+++ core/src/main/java/com/tw/core/entity/Item.java
@@ -0,0 +1,91 @@
+package com.tw.core.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by hgwang on 5/12/15.
+ */
+
+@Entity
+@Table(name="item")
+public class Item {
+
+    @Id
+    @Column(name="ID")
+    @GeneratedValue
+    private int id;
+
+    @Column(name="NAME")
+    private String name;
+
+    @Column(name="PRICE")
+    private double price;
+
+    @Column(name="UNIT")
+    private String unit;
+
+    @Column(name="CREATE_DATE")
+    private String create_data;
+
+    @Column(name="BAND")
+    private String band;
+
+    @Column(name="CATEGORY")
+    private String category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+    
+    public String getCreate_data() {
+        return create_data;
+    }
+
+    public void setCreate_data(String create_data) {
+        this.create_data = create_data;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
