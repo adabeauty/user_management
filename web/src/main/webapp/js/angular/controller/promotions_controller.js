7889a99263ecde48f8faad426719c120653e9a4b
hawang <654296217@qq.com>
2015-05-12 11:02:36 +0800

    implement jump to new_promotion page and show log bar

--- web/src/main/webapp/js/angular/controller/promotions_controller.js
+++ web/src/main/webapp/js/angular/controller/promotions_controller.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('userManagement')
-    .controller('promotionsController', function(){
+    .controller('promotionsController', function($scope, $http, $location){
 
+        log_out_nav_bar();
+        is_log_in();
+
+        function is_log_in(){
+
+            $http.get('/web/api/session').success(function(result){
+                if(result){
+                    $scope.Admin.name = result.user.name;
+                    $scope.logIn = true;
+                    $scope.logOut = false;
+                }
+            });
+        }
+        function log_out_nav_bar(){
+            $scope.Admin = {};
+            $scope.logIn = false;
+            $scope.logOut = true;
+        }
+
+        $scope.go_to_create_promotion = function(){
+            $location.path('new_promotion');
+        }
     });
