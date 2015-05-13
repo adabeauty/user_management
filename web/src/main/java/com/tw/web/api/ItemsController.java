d97fbfbf601cacbad5a71034eb8f6aaab0bd58dd
hawang <654296217@qq.com>
2015-05-13 16:12:03 +0800

    add getAllItemsWithPromotion method

--- web/src/main/java/com/tw/web/api/ItemsController.java
+++ web/src/main/java/com/tw/web/api/ItemsController.java
@@ -1,13 +1,16 @@
 package com.tw.web.api;
 
 import com.tw.core.ItemsService;
+import com.tw.core.PromotionsService;
 import com.tw.core.entity.Item;
+import com.tw.core.entity.Promotion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by hgwang on 5/12/15.
@@ -18,14 +21,25 @@ import java.util.List;
 public class ItemsController {
 
     private ItemsService itemsService;
+    private PromotionsService promotionsService;
 
     @Autowired
-    public ItemsController(ItemsService itemsService) {
+    public ItemsController(ItemsService itemsService, PromotionsService promotionsService) {
         this.itemsService = itemsService;
+        this.promotionsService = promotionsService;
     }
 
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List<Item> getAllItems(){
+//        return itemsService.getAllItems();
+//    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public List<Item> getAllItems(){
-        return itemsService.getAllItems();
+    public List<Map> getAllItemsWithPromotion(){
+
+        List<Item> items = itemsService.getAllItems();
+        List<Promotion> promotions = promotionsService.getAllPromotions();
+
+        return itemsService.getAllItemsWithPromotion(items, promotions);
     }
 }
